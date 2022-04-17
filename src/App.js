d468afb4d57ee7403d0816cfda451335ac056a45
Batuhan <yesiltas_sibel@hotmail.com>
2022-04-18 00:14:15 +0300

    v1

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
 import './App.css';
+import {useState} from "react";
 import FormComponent from './components/form/FormComponent';
 import Contacts from "./components/contacts/Contacts"
-import { useState } from 'react';
-import { AddUser } from './utils/functions';
+import { AddUser,EditUser } from './utils/functions';
+import { ToastContainer } from "react-toastify";
 
-const initialValues={
-  username:"",
-  phoneNumber:"",
-  gender:"NO ıNFO"
-}
+const initialValues={username:"",phoneNumber:"",gender:"NO INFO"}
 
 function App() {
-  const [info, setInfo] = useState(initialValues)
-   const handleFormSubmit=(e)=>{
-     e.preventDefault()
-     console.log(info)
-     AddUser(info)
-   }
-   const editHandler=(id,username,phoneNumber,gender)=>{
-    setInfo({id,username,phoneNumber,gender})
+ const [info,setInfo]=useState(initialValues);
+
+ const handleFormSubmit=(e)=>{
+   e.preventDefault();
+  if (info.id) {
+    EditUser(info)
   }
- 
+   else  {AddUser(info)}
+ }
+
+ const editHandler=(id,username,phoneNumber,gender)=>{
+   setInfo({id,username,phoneNumber,gender})
+ }
+
   return (
     <div className="App">
       <FormComponent info={info} setInfo={setInfo} handleFormSubmit={handleFormSubmit}/>
       <Contacts editHandler={editHandler}/>
+      <ToastContainer/>
     </div>
   );
 }
